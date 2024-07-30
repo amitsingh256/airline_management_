f167cee086c6b9956260a9cf28ff7d487638392a
Amit Singh <129637346+amitsingh256@users.noreply.github.com>
2024-07-30 11:20:54 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -9,6 +9,7 @@ import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.nio.channels.IllegalBlockingModeException;
 
 public class Login extends JFrame implements ActionListener {
 	JButton submit, reset, close;
@@ -16,40 +17,58 @@ public class Login extends JFrame implements ActionListener {
 	JPasswordField tfpassword;
 
 	public Login() {
+		
 //===========>>>>>>>>    FRAME SET   <<<<<<<<<========================
-		getContentPane().setBackground(Color.WHITE);
+//		getContentPane().setBackground(Color.WHITE);
+	
+		
+		ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("AirLine_Management/icons/login1.jpeg"));
+		JLabel image= new JLabel(i1);
+		image.setBounds(0,0,600,350);
+		add(image);
 		setLayout(null);
+		
+//============>>>>>>>>>>   HEADING <<<<<<<<<===================
+		Label heading = new Label(" LOGIN ");
+		heading.setFont(new Font("tahoma", Font.ITALIC, 16));
+		heading.setBounds(20,10,70,30);
+		heading.setForeground(Color.BLACK);
+		image.add(heading);
 //==========>>>>>>>>>   USERNAME PASSWORD LABEL SET <<<<<<<<<===============
 
 		JLabel lbusername = new JLabel("USER NAME");
-		lbusername.setBounds(120, 20, 100, 20);
-		add(lbusername);
+		lbusername.setBounds(70,50, 200,30);
+		lbusername.setFont(new Font("tahoma", Font.ITALIC,20));
+		lbusername.setForeground(Color.WHITE);
+    	image.add(lbusername);
 		tfusername = new JTextField();
-		tfusername.setBounds(230, 20, 200, 20);
-		add(tfusername);
+		tfusername.setBounds(200, 50, 250, 30);
+		image.add(tfusername);
 
 		JLabel lbpassword = new JLabel("PASSWORD");
-		lbpassword.setBounds(120, 60, 100, 20);
-		add(lbpassword);
+		lbpassword.setBounds(70, 90, 200, 30);
+		lbpassword.setFont(new Font("tahoma", Font.ITALIC,20));
+	    lbpassword.setForeground(Color.WHITE);
+		image.add(lbpassword);
 		tfpassword = new JPasswordField();
-		tfpassword.setBounds(230, 60, 200, 20);
-		add(tfpassword);
+		tfpassword.setBounds(200, 90, 250, 30);
+		image.add(tfpassword);
 
 //=============>>>>>>>>>>>      SET BUTTON   <<<<<<<<<<<===================		
 		submit = new JButton("SUBMIT");
-		submit.setBounds(140, 120, 120, 20);
+		submit.setBounds(140, 200, 120, 20);
 		submit.addActionListener(this);
-		add(submit);
+		image.add(submit);
 
 		reset = new JButton("RESET");
-		reset.setBounds(300, 120, 120, 20);
+		reset.setBounds(300, 200, 120, 20);
 		reset.addActionListener(this);
-		add(reset);
+		image.add(reset);
 
 		close = new JButton("CLOSE");
-		close.setBounds(240, 160, 120, 20);
+		close.setBounds(240, 260, 120, 20);
 		close.addActionListener(this);
-		add(close);
+		image.add(close);
 
 		setSize(600, 350);
 		setLocation(600, 250);
@@ -58,18 +77,22 @@ public class Login extends JFrame implements ActionListener {
 
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == submit) {
+
 			String username = tfusername.getText();
 			String password = tfpassword.getText();
-			try {
+      		try {
 				Connect c = new Connect();
-				String query = "SELECT * FROM LOGIN WHERE username = '" + username + "' and password = '" + password +"'";
+
+			String query = "SELECT * FROM LOGIN WHERE username = '" + username + "' and password = '" + password +"'";
 				ResultSet rs = c.stmt.executeQuery(query);
-				if (rs.next()) {
-					System.out.println("valid");
-				} else {
+		        if (rs.next()) {
+			     new Home();
+			     setVisible(false);
+				}else {
 					JOptionPane.showMessageDialog(null, "Invailid Username OR Password");
 					setVisible(false);
 				}
+				
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(null, "Invailid username OR password");
 			}
@@ -87,3 +110,4 @@ public class Login extends JFrame implements ActionListener {
 	}
 
 }
+
